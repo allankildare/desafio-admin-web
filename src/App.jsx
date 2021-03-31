eba228858c2d935b3535c4e85d2623cba4e5c0e7
Allan Kildare <allankildare27@gmail.com>
2021-03-31 02:37:16 -0300

    fix: redirecionamento via react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Redirect } from "react-router";
-// import logo from "./logo.svg";
+import { Redirect } from "react-router-dom";
 import "./App.css";
-import Dashboard from "./components/DashBoard/DashBoard";
 import { UserContext, LoginUser, AuthApp } from './context'
 
 function App() {
